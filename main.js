f80af35c9df1164b098ec977e4e0e579a3cab84d
laeeqah <laeeqahesau@gmail.com>
2021-02-15 13:09:22 +0200

    committed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
+// This function is for the clear button on the contact page.
 function reset() {
-    document.getElementsByClassName("form-input").value = "";
+  document.getElementsByClassName("form-input").value = "";
 }
-
